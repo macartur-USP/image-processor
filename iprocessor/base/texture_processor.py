9e8346497a67db4d9f551915f96ab3696b179a50
Macartur Sousa <macartur.sc@gmail.com>
2018-03-10 21:46:26 -0300

    Add procedure to calcula the local binary pattern

--- iprocessor/base/texture_processor.py
+++ iprocessor/base/texture_processor.py
@@ -1,7 +1,7 @@
 """Modulo to process texture."""
 from numpy import pi
 from skimage.filters import gabor_kernel
-from skimage.feature import greycomatrix
+from skimage.feature import greycomatrix, local_binary_pattern
 from .color_processor import ColorProcessor
 
 
@@ -19,3 +19,10 @@ class TextureProcessor:
         distances = [1]
         angles = [0, pi/6, pi/4, pi/3, pi/2, pi]
         return greycomatrix(image_grey, distances, angles, levels=levels)
+
+    @staticmethod
+    def local_binary_pattern(image):
+        """Execute the LBP to extract texture feature."""
+        image_grey = ColorProcessor.convert_rgb_to_grey(image)
+        return local_binary_pattern(image, P=8, R=1.0, method='default')
+
--- tests/base/test_texture_processor.py
+++ tests/base/test_texture_processor.py
@@ -29,3 +29,16 @@ class TestImageProcessor(unittest.TestCase):
                             [0, 2, 0, 0, 0, 0, 0, 0, 0],
                             [0, 0, 0, 0, 0, 1, 0, 0, 0]]
         self.assertTrue((result[:,:, 0, 0] == expected_angle_0).all())
+
+    def test_local_binary_pattern(self):
+        """Test whether the LBP method."""
+        image = np.array([[1,1,5,6,8],
+                          [2,3,5,7,1],
+                          [4,5,7,1,2],
+                          [8,5,1,2,5]], dtype=np.uint8)
+        result = TextureProcessor.local_binary_pattern(image)
+        expected_result = [[193., 243., 193., 65.,  0.],
+                           [193., 227., 103., 0., 126.],
+                           [193., 99., 0., 255., 104.],
+                           [0., 30., 63., 11., 0.]]
+        self.assertTrue(np.allclose(result, expected_result))
