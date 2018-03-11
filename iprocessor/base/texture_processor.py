37a1cb94e639265a5c205d46aa84078e904a1611
Macartur Sousa <macartur.sc@gmail.com>
2018-03-11 16:35:11 -0300

    Refactore texture processor
    Extract code and create methods: get_gabor_filter
                                     get_coocorrence_matrix_properties

--- iprocessor/base/texture_processor.py
+++ iprocessor/base/texture_processor.py
@@ -15,42 +15,52 @@ class TextureProcessor:
 
         This method will create texture features using:
 
-            - coocorrence_matrix:
-                - entropy (shannon entropy)
-                - contrast
-                - dissimilarity
-                - homogeneity
-                - ASM (segundo momento angula)
-                - energy
-                - correlation
-            - gabor filter:
-                - frequency: 1, 2**(1/5), 2
-                - theta = [0, 30, 45, 60, 90]
-            - entropy
             - local_binary_pattern:
-                - P=8
-                - R=1
-                - method='method'
+            - entropy
+            - coocorrence matrix
+            - gabor filter
         """
-
         entropy = TextureProcessor.entropy(image)
         lbp = TextureProcessor.local_binary_pattern(image)
-
-        props = ['contrast', 'homogeneity','dissimilarity', 'ASM',
-                 'correlation', 'energy']
-
-        matrix = TextureProcessor.coocorrence_matrix(image)
-        features = hstack([greycoprops(matrix, prop) for prop in props])
-
-        frequencies = [1, 2**(1/5), 2]
-        theta = [0, pi/6, pi/4, pi/3, pi/2, pi]
-        gabor_values = []
-        for freq in frequencies:
-            for t in  theta:
-                gabor_values.extend(hstack([TextureProcessor.gabor_filter(image, freq, t)]))
-        gabor = hstack(gabor_values)
+        features = TextureProcessor.get_coocorrence_matrix_properties(image)
+        gabor = TextureProcessor.get_gabor_properties(image)
 
         return hstack([features, entropy, lbp, gabor])
+
+    @staticmethod
+    def get_gabor_properties(image):
+        """ Get all properties from gabor filter.
+
+        - gabor filter with the parameters:
+            - frequency: 1, 2**(1/5), 2
+            - theta = [0, 30, 45, 60, 90]
+
+        """
+        frequencies = [1, 2**(1/5), 2]
+        theta = [0, pi/6, pi/4, pi/3, pi/2, pi]
+        gabor = []
+        for freq in frequencies:
+            for t in  theta:
+                gabor.extend(TextureProcessor.gabor_filter(image,freq, t))
+        return hstack(gabor)
+
+    @staticmethod
+    def get_coocorrence_matrix_properties(image):
+        """Create features based on image gray level.
+
+        The properties extracted are:
+            - contrast
+            - dissimilarity
+            - homogeneity
+            - ASM (Angular Second Moment)
+            - energy
+            - correlation
+        """
+        image_grey = ColorProcessor.convert_rgb_to_grey(image)
+        props = ['contrast', 'homogeneity','dissimilarity',
+                 'ASM', 'correlation', 'energy']
+        matrix = TextureProcessor.coocorrence_matrix(image_grey)
+        return hstack([greycoprops(matrix, prop) for prop in props])
 
     @staticmethod
     def coocorrence_matrix(image, levels=256):
